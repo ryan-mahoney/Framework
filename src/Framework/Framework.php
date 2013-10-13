ded6ee0842af1c75e37509864e928f17a0c6dd14
webuser <ryan@virtuecenter.com>
2013-10-13 02:06:33 -0400

    minor optimizations
    Former-commit-id: 505abda347aebfb4d8dd73d3a192991edb92b968

--- src/Framework/Framework.php
+++ src/Framework/Framework.php
@@ -13,66 +13,54 @@ class Framework {
 			}
 			$container->build->project($root);
 		}
-		$cache = $container->cache;
-		$collectionRoute = $container->collectionRoute;
-		$filter = $container->filter;
-		$helperRoute = $container->helperRoute;
-		$eventRoute = $container->eventRoute;
-		$formRoute = $container->formRoute;
 		$slim = $container->slim;
-		$imageResizer = $container->imageResizer;
-		$separation = $container->separation;
 		
 		//configuration cache
-		$items = [
-			$root . '-collections.json' => false,
-			$root . '-filters.json' => false,
-			$root . '-helpers.json' => false,
-			$root . '-events.json' => false
-		];
-		$result = $cache->getBatch($items);
+		$items = [$root . '-collections.json' => false, $root . '-filters.json' => false, $root . '-helpers.json' => false, $root . '-events.json' => false];
+		$result = $container->cache->getBatch($items);
 		if ($result === true) {
-			$collectionRoute->cacheSet(json_decode($items[$root . '-collections.json'], true));
-			$filter->cacheSet(json_decode($items[$root . '-filters.json'], true));
-			$helperRoute->cacheSet(json_decode($items[$root . '-helpers.json'], true));
-			$eventRoute->cacheSet(json_decode($items[$root . '-events.json'], true));
+			$container->collectionRoute->cacheSet(json_decode($items[$root . '-collections.json'], true));
+			$container->filter->cacheSet(json_decode($items[$root . '-filters.json'], true));
+			$container->helperRoute->cacheSet(json_decode($items[$root . '-helpers.json'], true));
+			$container->eventRoute->cacheSet(json_decode($items[$root . '-events.json'], true));
 		}
 
 		//smart routing
-		$slim::registerAutoloader();
 		$this->routeList($slim);
-		$this->collectionList($slim);
+		$container->helperRoute->helpers($root);
+		$container->collectionRoute->json($root);
+		$container->collectionRoute->pages($root);
+		$container->collectionRoute->collectionList($root);
+		$container->eventRoute->events($root);
+		$container->formRoute->json($root);
+		$container->formRoute->pages($root);
+		$container->imageResizer->route();
+		
+		//custom routing
 		$routePath = $root . '/Route.php';
 		if (!file_exists($routePath)) {
-    		exit('Route.php file undefined for site.');
+    		exit('Route.php file undefined for project.');
 		}
 		require $routePath;
 		if (!class_exists('\Route')) {
     		exit ('Route class not defined properly.');
 		}
-		$helperRoute->helpers($root);
-		$collectionRoute->json($root);
-		$collectionRoute->pages($root);
-		$eventRoute->events($root);
-		$formRoute->json($root);
-		$formRoute->pages($root);
-		$imageResizer->route();
 		$myRoute = new \Route($container);
 		if (method_exists($myRoute, 'custom')) {
 			$myRoute->custom();
 		}
-		
+
 		//generate output
 		ob_start();
 		$slim->run();
 		$return = ob_get_clean();
-		$filter->apply($root, $return);
+		$container->filter->apply($root, $return);
 		echo $return;
 	}
 
-	private function routeList ($app) {
-		$app->get('/routes', function () use ($app) {
-			$routes = $app->router()->getNamedRoutes();
+	private function routeList ($slim) {
+		$slim->get('/routes', function () use ($slim) {
+			$routes = $slim->router()->getNamedRoutes();
 			$paths = [];
 			echo '<html><body>';
 			foreach ($routes as $route) {
@@ -85,17 +73,5 @@ class Framework {
 			echo '</body></html>';
 			exit;
 		})->name('routes');
-	}
-
-	private function collectionList ($app) {
-		$app->get('/collections', function () use ($app) {
-			$collections = (array)json_decode(file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/collections/cache.json'), true);
-			echo '<html><body>';
-			foreach ($collections as $collection) {
-				echo '<a href="/json-data/' . $collection['p'] . '/all?pretty">', $collection['p'], '</a><br />';
-			}
-			echo '</body></html>';
-			exit;
-		})->name('collections');
 	}
 }
