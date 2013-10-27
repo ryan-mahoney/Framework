17dfa95b1d353ba9c0688ba9ce0ce3c82c1c063b
webuser <ryan@virtuecenter.com>
2013-10-27 10:40:04 -0400

    adjust container
    Former-commit-id: 741a4c389e3e5cfff09e99e3880b9825145d874d

--- src/Framework/Framework.php
+++ src/Framework/Framework.php
@@ -75,7 +75,7 @@ class Framework {
 		$container->formRoute->json();
 		$container->formRoute->app($root);
 		$container->managerRoute->app($root);
-		$container->bundleRoute->app();
+		//$container->bundleRoute->app();
 		
 		//custom routing
 		$routePath = $root . '/../Route.php';
