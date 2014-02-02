881c97813b31a34153a915cb8e3c180881107620
webuser <ryan@virtuecenter.com>
2014-02-02 14:50:48 -0500

    spaces

--- src/Framework.php
+++ src/Framework.php
@@ -26,161 +26,161 @@ namespace Opine;
 use Opine\Container;
 
 function container () {
-	return Framework::container();
+    return Framework::container();
 }
 
 class Framework {
-	private static $container;
-	private static $keyCache = [];
+    private static $container;
+    private static $keyCache = [];
 
-	public static function keySet ($name, $value) {
-		self::$keyCache[$name] = $value;
-	}
+    public static function keySet ($name, $value) {
+        self::$keyCache[$name] = $value;
+    }
 
-	public static function keyGet ($name) {
-		if (!isset(self::$keyCache[$name])) {
-			return false;
-		}
-		return self::$keyCache[$name];
-	}
+    public static function keyGet ($name) {
+        if (!isset(self::$keyCache[$name])) {
+            return false;
+        }
+        return self::$keyCache[$name];
+    }
 
-	public static function container () {
-		return self::$container;
-	}
+    public static function container () {
+        return self::$container;
+    }
 
-	public function frontController () {
-		$sapi = php_sapi_name();
-		$root = (($sapi == 'cli') ? getcwd() : $_SERVER['DOCUMENT_ROOT']);
-		if (substr($root, -6, 6) != 'public' && file_exists($root . '/public')) {
-			$root .= '/public';
-		}
-		self::$container = new Container($root, $root . '/../container.yml');
-		$container = self::$container;
-		if ($sapi == 'cli') {
-			if (!isset($_SERVER['argv'][1])) {
-				exit;
-			}
-			$command = $_SERVER['argv'][1];
-			switch ($command) {
-				case 'build':
-					$container->build->project($root);
-					exit;
-					break;
+    public function frontController () {
+        $sapi = php_sapi_name();
+        $root = (($sapi == 'cli') ? getcwd() : $_SERVER['DOCUMENT_ROOT']);
+        if (substr($root, -6, 6) != 'public' && file_exists($root . '/public')) {
+            $root .= '/public';
+        }
+        self::$container = new Container($root, $root . '/../container.yml');
+        $container = self::$container;
+        if ($sapi == 'cli') {
+            if (!isset($_SERVER['argv'][1])) {
+                exit;
+            }
+            $command = $_SERVER['argv'][1];
+            switch ($command) {
+                case 'build':
+                    $container->build->project($root);
+                    exit;
+                    break;
 
-				case 'worker':
-					set_time_limit(0);
-					$this->cache($root, $container);
-					$container->topic->load($root);
-					$container->worker->work();
-					exit;
-					break;
+                case 'worker':
+                    set_time_limit(0);
+                    $this->cache($root, $container);
+                    $container->topic->load($root);
+                    $container->worker->work();
+                    exit;
+                    break;
 
-				case 'upgrade':
-					$container->build->upgrade($root);
-					exit;
-					break;
+                case 'upgrade':
+                    $container->build->upgrade($root);
+                    exit;
+                    break;
 
-				case 'check':
-					$container->build->environmentCheck($root);
-					exit;
+                case 'check':
+                    $container->build->environmentCheck($root);
+                    exit;
 
-				case 'dburi':
-				    $container->dbmigration->addURI();
-				    exit;
+                case 'dburi':
+                    $container->dbmigration->addURI();
+                    exit;
 
-				case 'reindex':
-					exit;
+                case 'reindex':
+                    exit;
 
-				case 'topics':
-					$this->cache($root, $container);
-					$container->topic->load($root);
-					$container->topic->show();
-					exit;
+                case 'topics':
+                    $this->cache($root, $container);
+                    $container->topic->load($root);
+                    $container->topic->show();
+                    exit;
 
-				case 'count':
-					$container->collection->statsAll();
-					exit;
-			}
-			exit;
-		}
-		if (strlen(session_id()) == 0) {
-    		session_start();
-		}
-		$slim = $container->slim;
-		if (isset($_POST) && !empty($_POST)) {
-			$container->post->populate($slim->request->getResourceUri(), $_POST);
-		}
-		
-		//configuration cache
-		$this->cache($root, $container);
+                case 'count':
+                    $container->collection->statsAll();
+                    exit;
+            }
+            exit;
+        }
+        if (strlen(session_id()) == 0) {
+            session_start();
+        }
+        $slim = $container->slim;
+        if (isset($_POST) && !empty($_POST)) {
+            $container->post->populate($slim->request->getResourceUri(), $_POST);
+        }
+        
+        //configuration cache
+        $this->cache($root, $container);
 
-		//smart routing
-		$container->imageResizer->route();
-		$container->helperRoute->helpers($root);
-		$container->collectionRoute->json($root);
-		$container->collectionRoute->app($root);
-		$container->collectionRoute->collectionList($root);
-		$container->formRoute->json();
-		$container->formRoute->app($root);
-		$container->topic->load($root);
-		$container->bundleRoute->app($root);
-		$container->authentication->aclRoute();
-		
-		//custom routing
-		$routePath = $root . '/../Route.php';
-		if (!file_exists($routePath)) {
-    		exit('Route.php file undefined for project.');
-		}
-		require $routePath;
-		if (!class_exists('\Route')) {
-    		exit ('Route class not defined properly.');
-		}
-		$myRoute = new \Route($container);
-		if (method_exists($myRoute, 'custom')) {
-			$myRoute->custom();
-		}
+        //smart routing
+        $container->imageResizer->route();
+        $container->helperRoute->helpers($root);
+        $container->collectionRoute->json($root);
+        $container->collectionRoute->app($root);
+        $container->collectionRoute->collectionList($root);
+        $container->formRoute->json();
+        $container->formRoute->app($root);
+        $container->topic->load($root);
+        $container->bundleRoute->app($root);
+        $container->authentication->aclRoute();
+        
+        //custom routing
+        $routePath = $root . '/../Route.php';
+        if (!file_exists($routePath)) {
+            exit('Route.php file undefined for project.');
+        }
+        require $routePath;
+        if (!class_exists('\Route')) {
+            exit ('Route class not defined properly.');
+        }
+        $myRoute = new \Route($container);
+        if (method_exists($myRoute, 'custom')) {
+            $myRoute->custom();
+        }
 
-		//generate output
-		$this->routeList($slim);
-		$slim->run();
-		$container->filter->apply($root);
-		echo $container->response;
-	}
+        //generate output
+        $this->routeList($slim);
+        $slim->run();
+        $container->filter->apply($root);
+        echo $container->response;
+    }
 
-	private function cache ($root, $container) {
-		$items = [
-			$root . '-collections.json' => false,
-			$root . '-filters.json' => false,
-			$root . '-forms.json' => false,
-			$root . '-bundles.json' => false,
-			$root . '-topics.json' => false,
-			$root . '-acl.json' => false
-		];
-		$result = $container->cache->getBatch($items);
-		if ($result === true) {
-			$container->collectionRoute->cacheSet(json_decode($items[$root . '-collections.json'], true));
-			$container->filter->cacheSet(json_decode($items[$root . '-filters.json'], true));
-			$container->formRoute->cacheSet(json_decode($items[$root . '-forms.json'], true));
-			$container->bundleRoute->cacheSet(json_decode($items[$root . '-bundles.json'], true));
-			$container->topic->cacheSet(json_decode($items[$root . '-topics.json'], true));
-			$container->authentication->cacheSet(json_decode($items[$root . '-acl.json'], true));
-		}
-	}
+    private function cache ($root, $container) {
+        $items = [
+            $root . '-collections.json' => false,
+            $root . '-filters.json' => false,
+            $root . '-forms.json' => false,
+            $root . '-bundles.json' => false,
+            $root . '-topics.json' => false,
+            $root . '-acl.json' => false
+        ];
+        $result = $container->cache->getBatch($items);
+        if ($result === true) {
+            $container->collectionRoute->cacheSet(json_decode($items[$root . '-collections.json'], true));
+            $container->filter->cacheSet(json_decode($items[$root . '-filters.json'], true));
+            $container->formRoute->cacheSet(json_decode($items[$root . '-forms.json'], true));
+            $container->bundleRoute->cacheSet(json_decode($items[$root . '-bundles.json'], true));
+            $container->topic->cacheSet(json_decode($items[$root . '-topics.json'], true));
+            $container->authentication->cacheSet(json_decode($items[$root . '-acl.json'], true));
+        }
+    }
 
-	private function routeList ($slim) {
-		$slim->get('/routes', function () use ($slim) {
-			$routes = $slim->router()->getNamedRoutes();
-			$paths = [];
-			echo '<html><body>';
-			foreach ($routes as $route) {
-				$pattern = $route->getPattern();
-				if (substr_count($pattern, '(')) {
-					$pattern = explode('(', $pattern, 2)[0];
-				}
-				echo '<a href="', $pattern, '">', $route->getName(), '</a><br />';
-			}
-			echo '</body></html>';
-			exit;
-		})->name('routes');
-	}
+    private function routeList ($slim) {
+        $slim->get('/routes', function () use ($slim) {
+            $routes = $slim->router()->getNamedRoutes();
+            $paths = [];
+            echo '<html><body>';
+            foreach ($routes as $route) {
+                $pattern = $route->getPattern();
+                if (substr_count($pattern, '(')) {
+                    $pattern = explode('(', $pattern, 2)[0];
+                }
+                echo '<a href="', $pattern, '">', $route->getName(), '</a><br />';
+            }
+            echo '</body></html>';
+            exit;
+        })->name('routes');
+    }
 }
