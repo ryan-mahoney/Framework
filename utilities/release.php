988478884bd54e0ed6d683af9c51d97898e3944a
webuser <ryan@virtuecenter.com>
2014-10-23 01:55:29 -0400

    smarter release approach

--- utilities/release.php
+++ utilities/release.php
@@ -1,7 +1,7 @@
 <?php
-foreach (new DirectoryIterator('./') as $fileInfo) {
+foreach (new DirectoryIterator(__DIR__ . '/../vendor/opine') as $fileInfo) {
     if ($fileInfo->isDot()) continue;
     if (!$fileInfo->isDir()) continue;
-    $hash = trim(shell_exec('cd ./' . $fileInfo->getFilename() . ' && git rev-parse HEAD'));
+    $hash = trim(shell_exec('cd ' . __DIR__ . '/../vendor/opine/' . $fileInfo->getFilename() . ' && git rev-parse HEAD'));
     echo '"opine/', $fileInfo->getFilename(), '": "dev-master#', $hash, '",', "\n";
 }
