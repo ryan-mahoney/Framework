a8dd89db0f52e14463a3517745b23db2dd200ddf
Ryan Mahoney <ryan@virtuecenter.com>
2014-11-08 22:37:25 -0500

    Update provision.sh

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -11,41 +11,12 @@ sudo echo "deb http://packages.elasticsearch.org/elasticsearch/1.3/debian stable
 # update
 sudo apt-get update 2> /dev/null
 
-# install services
-sudo apt-get install -y wget curl 2> /dev/null
-sudo apt-get install -y make autoconf 2> /dev/null
-sudo apt-get install -y vim 2> /dev/null
-sudo apt-get install -y nano 2> /dev/null
-sudo apt-get install -y mlocate 2> /dev/null
-sudo apt-get install -y nginx 2> /dev/null
-sudo apt-get install -y openssl 2> /dev/null
-sudo apt-get install -y default-jre 2> /dev/null
-sudo apt-get install -y mongodb-server 2> /dev/null
-sudo apt-get install -y elasticsearch 2> /dev/null
-sudo apt-get install -y git 2> /dev/null
-sudo apt-get install -y memcached libmemcached-tools 2> /dev/null
-sudo apt-get install -y beanstalkd 2> /dev/null
-sudo apt-get install -y libmcrypt-dev libxml2-dev libtidy-dev libzip-dev libgd2-xpm-dev libcurl4-openssl-dev libyaml-dev libevent-dev 2> /dev/null
-sudo apt-get install -y php5 php5-cli php5-fpm php-pear php5-dev php5-gd php5-curl php5-mcrypt php5-curl php5-tidy php5-mongo php5-memcache 2> /dev/null
-
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password ROOTPASSWORD' 2> /dev/null
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password ROOTPASSWORD' 2> /dev/null
-sudo apt-get install -y mysql-server 2> /dev/null
-sudo apt-get install -y mysql-client 2> /dev/null
-
-# mysql configuration
-if [ ! -f /var/log/dbinstalled ];
-then
-    echo "CREATE USER 'mysqluser'@'localhost' IDENTIFIED BY 'USERPASSWORD'" | mysql -uroot -pROOTPASSWORD
-    echo "CREATE DATABASE internal" | mysql -uroot -pROOTPASSWORD
-    echo "GRANT ALL ON internal.* TO 'mysqluser'@'localhost'" | mysql -uroot -pROOTPASSWORD
-    echo "flush privileges" | mysql -uroot -pROOTPASSWORD
-fi
+# install software
+sudo apt-get install -y wget curl make autoconf vim nano mlocate nginx openssl default-jre mongodb-server elasticsearch git memcached libmemcached-tools beanstalkd php5 php5-cli php5-fpm php-pear php5-dev php5-gd php5-curl php5-mcrypt php5-curl php5-tidy php5-mongo php5-memcache 2> /dev/null
 
 # php configuration
-printf "\n" | sudo pecl install yaml
+sudo pecl install yaml
 sudo echo "extension=yaml.so" | sudo tee -a /etc/php5/fpm/php.ini
-
 php5enmod mcrypt
 
 # php composer
@@ -56,7 +27,8 @@ sudo mv composer.phar /usr/local/bin/composer
 sed -i 's/www\-data/vagrant/' /etc/nginx/nginx.conf
 sed -i 's/www\-data/vagrant/' /etc/php5/fpm/pool.d/www.conf
 sudo mkdir /var/www
+mkdir /var/www/storage
+mkdir /var/www/imagecache
 sudo ln -s /var/www/project/server/vhost.conf /etc/nginx/sites-enabled/vhost.conf
 sudo /etc/init.d/nginx restart
-
-# opine cli tools
+ln -s /usr/bin/php /usr/local/bin/php
