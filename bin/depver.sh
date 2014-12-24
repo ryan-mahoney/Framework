3617c96ace54a1ef0f7524d9b044d6fbc0e381ba
Ryan Mahoney <ryan@virtuecenter.com>
2014-12-24 00:44:54 -0500

    new versioning tool for depenedencies

---
+++ bin/depver.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env php
+<?php
+//usage .framework/bin/depver.sh "opine/bundle" "1.9.0" test
+if (!isset($argv[1])) {
+    die('first argument is component' . "\n");
+}
+if (!isset($argv[2])) {
+    die('seconf argument is new version' . "\n");
+}
+$mode = 'test';
+if (isset($argv[3])) {
+    $mode = $argv[3];
+}
+$component = $argv[1];
+$version = $argv[2];
+
+$cmd = "cd " . __DIR__ . "/../../ && find . | grep composer.json | grep -v bower | grep -v vagrant | sed 's/\.\//" . str_replace('/', '\/', __DIR__ . '/../../') . "/'";
+$composers = explode("\n", trim(shell_exec($cmd)));
+$pattern = '/("' . str_replace('/', '\/', $component) . '")( *\: *)("[\.0-9]*")/';
+foreach ($composers as $composer) {
+    $content = file_get_contents($composer);
+    if (substr_count($content, $component) < 1) {
+        continue;
+    }
+    $newContent = preg_replace($pattern, '$1$2"' . $version . '"', $content);
+    if ($mode == 'test') {
+        echo $composer, "\n";
+        echo $newContent, "\n\n";
+    } else {
+        file_put_contents($composer, $newContent);
+    }
+}
